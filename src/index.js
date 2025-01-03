0d02d6bcd17a779de5bd00c56cd7cea8569197ab
MX-GΔMΞCØDΞR <150932410+themxgamecoder@users.noreply.github.com>
2025-01-03 21:57:44 +0100

    hello world

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const { 
+  merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly,
+  webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl, 
+  apkmirror, weather, apksearch, happymod, nickff, nickml, 
+  mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, 
+  konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
+  tiktokDl, xdown, mediafire, googleImage, gitclone
+} = require('./mx-api');
+const { igdl } = require('./insta');
+const { fbdl } = require('./fb');
+
+const Qasim = {
+  merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly,
+  webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl,
+  apkmirror, weather, apksearch, happymod, nickff, nickml, 
+  mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, 
+  konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
+  tiktokDl, xdown, igdl, fbdl, mediafire, googleImage, gitclone
+};
+
+module.exports = Qasim;
